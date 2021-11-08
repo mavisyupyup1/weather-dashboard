e8f70e2bb1306379eb4c2e8cccd37f535e469757
Your Name <graceliu@gracelius-iMac.home>
2021-11-08 10:52:45 -0500

    search history city and load last by default

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,23 +4,22 @@ var getWeatherBtn = document.querySelector("#get-weather-btn");
 var displayCurrentWeatherEl =document.querySelector(".current-container")
 var displayForecastEl = document.querySelector(".forecast-container");
 var displayHistoryEl = document.querySelector("#history-city");
-
+var searchHistoryList = [];
 
 var formSubmitHandler = function(event){
     event.preventDefault();
     var cityName = cityInputEl.value.trim();
     if(cityName){
         getWeatherByCity(cityName);
-        cityInputEl.value="";
-        displayCurrentWeatherEl.textContent="" ;
-        displayForecastEl.textContent="";
-        localStorage.setItem("city",cityName);
-        var historyCity = localStorage.getItem("city");
-        var historyCityBtn = document.createElement("btn")
-        historyCityBtn.className ="btn btn-light";
-        historyCityBtn.textContent = historyCity;
-        displayHistoryEl.append(historyCityBtn);
-
+        if(!searchHistoryList.includes(cityName)){
+            searchHistoryList.push(cityName);
+       var historyCityBtn = document.createElement("list-group-item")
+       historyCityBtn.className ="btn btn-light list-group-item";
+       historyCityBtn.textContent = cityName;
+       displayHistoryEl.append(historyCityBtn);
+        }
+        localStorage.setItem("city", JSON.stringify(searchHistoryList));
+        console.log(searchHistoryList);
     }
     else{
         alert("Please enter a city name.")
@@ -60,6 +59,9 @@ var displayWeather =function(weather,oneCallWeather){
 
 //     WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+cityInputEl.value="";
+        displayCurrentWeatherEl.textContent="" ;
+        displayForecastEl.textContent="";
 var cityDisplayName =  weather.name;
 var currentDate = moment.unix(weather.dt).format("MM/DD/YYYY");
 var cityDisplayNameEl = document.createElement("h2");
@@ -70,6 +72,9 @@ displayCurrentWeatherEl.appendChild(cityDisplayNameEl);
 var temperature = weather.main.temp;
 var wind =  weather.wind.speed;
 var humidity = weather.main.humidity;
+console.log(humidity);
+var uvIndex= oneCallWeather.current.uvi;
+console.log(uvIndex);
 var temperatureEl = document.createElement("p");
 temperatureEl.textContent = "Temp: "+ temperature + " °F";
 displayCurrentWeatherEl.appendChild(temperatureEl);
@@ -81,6 +86,10 @@ displayCurrentWeatherEl.appendChild(windEl);
 var humidityEl = document.createElement("p");
 humidityEl.textContent = "Humidity: "+ humidity + " %";
 displayCurrentWeatherEl.appendChild(humidityEl);
+
+var uvIndexEl = document.createElement("p");
+uvIndexEl.textContent = "UV Index: "+uvIndex;
+displayCurrentWeatherEl.appendChild(uvIndexEl);
 
 for (var i=2;i<7;i++){
 
@@ -96,9 +105,9 @@ var iconUrl = `<img src="https://openweathermap.org/img/w/${cityForecast.icon}.p
 
 //var forecastCardEl = document.querySelector(".card-"+[i])
 var forecastDateDisplay = document.createElement("card")
-forecastDateDisplay.className = "card bg-blue text-light p-2 my-0"
-forecastDateDisplay.innerHTML= cityForecast.date + iconUrl
-+ "<p><font size=2>Temp: "+cityForecast.temp+ " °F<br><font><p>"+ "<p>Wind: "+cityForecast.wind+" MPH<p>"+ "<p>Humidity: "+cityForecast.humidity+" %<p>";
+forecastDateDisplay.className = "card bg-blue text-light p-2 my-0 col-2 "
+forecastDateDisplay.innerHTML= "<h5>"+cityForecast.date+"</h5>" + "<img>"+iconUrl+"</img>"
++ "<p>Temp: "+cityForecast.temp+ " °F<br><font><p>"+ "<p>Wind: "+cityForecast.wind+" MPH<p>"+ "<p>Humidity: "+cityForecast.humidity+" %<p>";
 displayForecastEl.appendChild(forecastDateDisplay);
 
 }
@@ -109,4 +118,22 @@ displayForecastEl.appendChild(forecastDateDisplay);
 };
 
 getWeatherBtn.addEventListener("click",formSubmitHandler);
-historyCityBtn.addEventListener("click",formSubmitHandler(cityName));
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+$(document).on("click", ".list-group-item", function() {
+    var listCity = $(this).text();
+   getWeatherByCity(listCity);
+});
+
+// WHEN I open the weather dashboard
+// THEN I am presented with the last searched city forecast
+$(document).ready(function() {
+    var searchHistoryArr = JSON.parse(localStorage.getItem("city"));
+    console.log(searchHistoryArr);
+    if (searchHistoryArr !== null) {
+        var lastSearchedIndex = searchHistoryArr.length - 1;
+        var lastSearchedCity = searchHistoryArr[lastSearchedIndex];
+       getWeatherByCity(lastSearchedCity);
+        console.log(`Last searched city: ${lastSearchedCity}`);
+    }
+});
